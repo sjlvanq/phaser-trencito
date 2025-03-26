cb68efd8cc102ebf910520eabeeb5acbc1c55fec
sjlvanq <sjlvanq@gmail.com>
2025-03-26 02:17:48 -0300

    feat: incluir la tecla Enter para la selección del botón principal o ítem de menú por defecto

--- utils/teclado.js
+++ utils/teclado.js
@@ -1,7 +1,9 @@
 export function establecerBotonPorDefecto(scene, boton) {
-	scene.input.keyboard?.on('keydown-SPACE', function() {
-		if (boton.input?.enabled){ 
-			boton.emit('pointerdown');
+	scene.input.keyboard?.on('keydown', (event) => {
+		if(event.code === 'Space' || event.code === 'Enter') {
+			if (boton.input?.enabled){ 
+				boton.emit('pointerdown');
+			}
 		}
 	});
 }
